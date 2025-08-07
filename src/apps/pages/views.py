d44059139ebf16191e3c7c57bb5f887924aa08f1
Nick Salvatore <nmsalvatore@gmail.com>
2025-08-07 16:23:38 -0700

    feat: rendered otp form field after email submit

--- src/apps/pages/views.py
+++ src/apps/pages/views.py
@@ -1,5 +1,24 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get("email")
+        passcode = request.POST.get("passcode")
+
+        if email and not passcode:
+            # TODO: send code
+
+            if request.headers.get("HX-Request"):
+                return render(request, "pages/partials/otp_form.html", {
+                    "email": email
+                })
+
+            else:
+                return render(request, self.template_name, {
+                    "otp_sent": True,
+                    "email": email,
+                })
