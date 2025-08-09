eb9a738f8c42c2189c84e8d6adc5d00eb5425e55
Nick Salvatore <nmsalvatore@gmail.com>
2025-08-08 19:10:43 -0700

    feat: created urls.py for pages app, included in config urls

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from src.apps.pages import views as pages_views
-
 urlpatterns = [
-    path("", pages_views.HomeView.as_view(), name="home"),
+    path("", include("src.apps.pages.urls")),
     path("admin/", admin.site.urls),
 ]
