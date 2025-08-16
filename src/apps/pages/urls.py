776ce67f6adfca3490933cac691e4a4b2faef0c0
Nick Salvatore <nmsalvatore@gmail.com>
2025-08-16 15:38:39 -0700

    refactor: updated view import paths to correspond with view directory creation

--- src/apps/pages/urls.py
+++ src/apps/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import FrontPageView, DashboardView
+from .views.front_page import FrontPageView
+from .views.dashboard import DashboardView
 
 app_name = "pages"
 
