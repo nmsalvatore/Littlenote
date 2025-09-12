d4a3ee699859475be8c8acec0a1bff63143cc885
Nick Salvatore <nmsalvatore@gmail.com>
2025-09-12 14:00:17 -0700

    feat: elegant closure of delete dialog, minor dialog styles

--- src/apps/notes/static/notes/js/delete.js
+++ src/apps/notes/static/notes/js/delete.js
@@ -2,15 +2,15 @@
     const deleteNoteButtonId = "delete_note_button";
     const deleteNoteButton = document.getElementById(deleteNoteButtonId);
 
+    const deleteNoteDialogId = "delete_note_dialog";
+    const deleteNoteDialog = document.getElementById(deleteNoteDialogId);
+
     if (!deleteNoteButton) {
         console.error(`Could not find button with ID "${deleteNoteButtonId}".`);
         return;
     }
 
     deleteNoteButton.addEventListener("click", () => {
-        const deleteNoteDialogId = "delete_note_dialog";
-        const deleteNoteDialog = document.getElementById(deleteNoteDialogId);
-
         if (!deleteNoteDialog) {
             console.error(
                 `Could not find dialog with ID "${deleteNoteDialogId}".`,
@@ -19,5 +19,11 @@
         }
 
         deleteNoteDialog.showModal();
+    });
+
+    deleteNoteDialog.addEventListener("click", (e) => {
+        if (e.target === deleteNoteDialog) {
+            deleteNoteDialog.close();
+        }
     });
 })();
