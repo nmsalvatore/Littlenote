9e81391004844b0d0217419d1344618a61de3515
Nick Salvatore <nmsalvatore@gmail.com>
2025-09-14 07:13:56 -0700

    feat: initialize journal app, conditionally add active class on nav links

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("src.apps.pages.urls")),
+    path("journal/", include("src.apps.journal.urls")),
     path("notes/", include("src.apps.notes.urls")),
     path("accounts/", include("src.apps.accounts.urls")),
     path("admin/", admin.site.urls),
